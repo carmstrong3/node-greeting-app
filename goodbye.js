558db60c521e50f4f32ab8877eb5c2381183410c
Clifford Armstrong III <clifford.armstrong.3@gmail.com>
2018-10-19 12:28:07 -0700

    fin

---
+++ goodbye.js
@@ -0,0 +1,17 @@
+exports.goodbyeEnglish = () => {
+  console.log("Goodbye")
+}
+
+exports.goodbyeJapanese = () => {
+  console.log("Jaa ne")
+}
+
+exports.goodbyeSpanish = () => {
+  console.log("Adios")
+}
+
+exports.goodbyeIcelandic = () => {
+  console.log("Vertu saell")
+}
+
+
---
+++ main.js
@@ -0,0 +1,15 @@
+const greetings = require("./greeting.js");
+const goodbyes = require("./goodbye.js");
+
+greetings.greetingEnglish();
+goodbyes.goodbyeEnglish();
+
+greetings.greetingJapanese();
+goodbyes.goodbyeJapanese();
+
+greetings.greetingSpanish();
+goodbyes.goodbyeSpanish();
+
+greetings.greetingIcelandic();
+goodbyes.goodbyeIcelandic();
+
